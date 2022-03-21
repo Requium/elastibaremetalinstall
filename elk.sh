92ebbdce416fa4f088d274002334ce164ffef403
Requium <30955371+Requium@users.noreply.github.com>
2022-03-21 12:12:23 -0400

    Update elk.sh

--- elk.sh
+++ elk.sh
@@ -2,3 +2,5 @@ curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt update
 sudo apt install elasticsearch
+curl -XGET 'http://localhost:9200/filebeat-*/_search?pretty'
+
